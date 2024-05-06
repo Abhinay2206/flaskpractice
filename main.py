8afecbed92d888a05c2aab3f18d0f5d102436524
Abhinay Bakkera <134620717+Abhinay2206@users.noreply.github.com>
2024-05-06 15:47:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+JOBS = [{
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengalure, India',
+    'salary': 'Rs. 1,00,000',
+}, {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 15,00,000',
+}, {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote',
+    'salary': 'Rs. 12,00,000',
+}, {
+    'id': 3,
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, USA',
+    'salary': '$120,000',
+}]
+
+
+@app.route("/")
+def hello_world():
+    return render_template("home.html", jobs=JOBS)
+
+
+@app.route("/apifrom flask import Flask, jsonify, render_template/jobs")
+def list_jobs():
+    return jsonify(JOBS)
+
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', debug=True)
